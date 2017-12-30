0c0efd87d071516ad2836365cf8128b885098731
Zakary Kurzawski <zakarykurzawski@Zakarys-MacBook-Pro.local>
2017-12-29 23:19:38 -0600

    built login screen

---
+++ smack-app/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  smack-app
+//
+//  Created by Zakary Kurzawski on 12/29/17.
+//  Copyright © 2017 Zakary Kurzawski. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ smack-app/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  smack-app
+//
+//  Created by Zakary Kurzawski on 12/29/17.
+//  Copyright © 2017 Zakary Kurzawski. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
